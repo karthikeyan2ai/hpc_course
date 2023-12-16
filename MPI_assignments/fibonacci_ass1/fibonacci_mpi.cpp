5501e8cf9ea3d419c44f37bc8208af038bc76a5d
Karthikeyan M <karthi6019@gmail.com>
2023-12-16 20:44:41 +0530

    Update fibonacci_mpi.cpp
    Added calculation time

--- MPI_assignments/fibonacci_ass1/fibonacci_mpi.cpp
+++ MPI_assignments/fibonacci_ass1/fibonacci_mpi.cpp
@@ -19,10 +19,18 @@ int main(int argc, char** argv) {
     int local_start = rank * (n / size);
     int local_end = (rank + 1) * (n / size);
 
+    double start_time = MPI_Wtime(); 
+
     for (int i = local_start; i < local_end; i++) {
         int result = fibonacci(i);
         std::cout << "Process " << rank << ": Fibonacci(" << i << ") = " << result << std::endl;
     }
+
+    double end_time = MPI_Wtime(); 
+
+    double elapsed_time = end_time - start_time; // Calculate time
+
+    std::cout << "Process " << rank << ": Elapsed time = " << elapsed_time << " seconds" << std::endl;
 
     MPI_Finalize();
     return 0;
